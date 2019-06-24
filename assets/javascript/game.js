b0d5a2aeef7ab745da5318d497d01c3e39b5e3f5
Tina Azad <mktazad@gmail.com>
2019-06-24 17:30:35 -0400

    HW

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,24 @@
 //user will guess number
 //compare user's guess 
 //if user's win, win number increment
-//if user loose, loose  number increment
+//if user losses, loss  number increment
 $(document).ready(function(){
 var guessLetters =[];//letters that have been guessed so far
 
 var wins = 0;//number of wins so far
 var losses = 0;//number of losses so far
 
-var numberOfGuessesLeft = 10; //number of intial guesses
-
+var numberOfGuessesLeft = 9; //number of intial guesses
 var computerLetter = "";
 
 //aray of letters = ["a","b","c","d"];
 
 ////////////////////////////////////////////////////
 //computer generate random letter
-var arrayOfLetters = ["a","b","c","d","e"];
+var arrayOfLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lengthOfLetters = arrayOfLetters.length;
+var randomLetter = arrayOfLetters[Math.floor(Math.random() * lengthOfLetters)];
 
-var lengthofletters = arrayOfLetters.length;
-
-var randomLetter = arrayOfLetters[Math.floor(Math.random() * lengthofletters)];
 ////////////////////////////////////////////
 
 console.log(randomLetter);
@@ -30,12 +28,20 @@ console.log(randomLetter);
 document.onkeydown = function(event){
 
     //capture key press and make it lowercase
-    var userguess = event.key.toLowerCase();
+    console.log("keypressed")
+    var userGuess = event.key.toLowerCase();
+    if (!arrayOfLetters.includes(userGuess)) {
+        console.log("not a letter")
+        return false;
+    }
 
-    guessLetters.push(userguess);
-
+    guessLetters.push(userGuess);
     //after every guess, decrease number of guesses left by 1
-    numberOfGuessesLeft--;
+    if (numberOfGuessesLeft > 0) {
+        numberOfGuessesLeft--;
+    }
+    //after every guess, decrease number of guesses left by 1
+    //numberOfGuesses: null;
 
 
     //userguess
@@ -43,26 +49,47 @@ document.onkeydown = function(event){
 
 
     //user guessed correctly
-    if(userguess == randomLetter){
-        wins++;
+     
+    if (numberOfGuessesLeft > 0){
+        if(userGuess == randomLetter){
+            wins++;
+            alert("Wow! congratulation! Try another!");
+            reset();
+        }
+
     }
+    //}
     //user guessed incorrectly
     else if(numberOfGuessesLeft == 0){
         losses++;
+        alert(" Sorry, you're not psychic.. try again!");
+        reset();
     }
-
+    
     updatePage();
 };
+
+function reset() {
+    numberOfGuessesLeft = 9;
+    randomLetter = arrayOfLetters[Math.floor(Math.random() * lengthOfLetters)];
+    guessLetters =[];
+    updatePage();
+}
 
 var updatePage = function(){
     $("#wins").text(wins);
     $("#losses").text(losses);
     $("#Guesses").text(numberOfGuessesLeft);
-    $("#yourGuesseSoFar").text(guessLetters.toString());
+    $("#yourGuessesSoFar").text(guessLetters.toString());
+    
 };
 
-updatePage();
+updatePage(); 
+
+
 
 });
 //random number = 1;
-// of letarrayter[random number]
+// arrayOfLetter[random number]
+
+
